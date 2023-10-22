7b235d358f4dcd7f251e68bdc3bd987e47f1d24d
Janvi <jg.gupta.janvi@gmail.com>
2023-10-23 00:55:21 +0530

    changes

---
+++ BookStoreWebsite/src/com/bookstore/controller/admin/ListUsersServlet.java
@@ -0,0 +1,28 @@
+package com.bookstore.controller.admin;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/admin/list_users")
+public class ListUsersServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public ListUsersServlet() {
+    	super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
